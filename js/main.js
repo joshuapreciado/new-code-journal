0987d18840c12cb043c17b0e46c1114fd1d09d65
joshuapreciado <preciadojoshua340@yahoo.com>
2022-07-11 19:29:32 -0700

    Completed user can create an entry

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 //Listen for input events & Change src attribute of img element
 var photoholder = document.querySelector('.phototext');
-var fakeimage = document.querySelector('.standardimage')
+var fakeimage = document.querySelector('.standard-image')
 var formelement = document.querySelector('form');
 
 photoholder.addEventListener('input', inputhandler);
@@ -18,24 +18,17 @@ function falseyimage(url) {
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 }
 
-//Listen for submit events on journal ent.form and
-// Put the form's input values into a new object. CHECK
-// Add the nextEntryId to the object. CHECK
-// Increment the nextEntryId on the data model. CHECK
-// Prepend the new object to the entries in the data model. CHECK
-// Reset the image preview's `src' attribute.
-// Reset the form inputs.
 var entryform = document.querySelector('#entry-form');
 entryform.addEventListener('submit', function(event) {
   event.preventDefault();
   var inputvalues = {};
-  inputvalues.titletext = formelement.elements.titletext.value;
-  inputvalues.phototext = formelement.elements.phototext.value;
-  inputvalues.textarea1 = formelement.elements.textarea1.value;
+  inputvalues.titleText = formelement.elements.titleText.value;
+  inputvalues.photoText = formelement.elements.photoText.value;
+  inputvalues.textArea1 = formelement.elements.textArea1.value;
 
-  inputvalues.id = data.nextEntryId;
+  inputvalues.entryId = data.nextEntryId;
   data.nextEntryId++;
-  data.prepend(inputvalues);
+  data.entries.unshift(inputvalues);
   fakeimage.src = 'images/placeholder-image-square.jpg';
   formelement.reset();
 })
