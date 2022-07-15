abec2626554653fcfb9cf0b3e43e44261a340af3
joshuapreciado <preciadojoshua340@yahoo.com>
2022-07-14 20:24:26 -0700

    Completed version

--- js/main.js
+++ js/main.js
@@ -101,27 +101,3 @@ newentry.addEventListener('click', function (event) {
   data.view = 'entry-form';
   viewswap('entry-form');
 });
-//Add some dummy journal entries to the unordered- Y
-//list in the HTML and style them to match the wireframe. Y
-// Check that the entries list looks good on both mobile and desktop screens. Y
-// Define a function that takes a single journal entry -
-//object and returns a DOM tree that matches one of the example entries in the HTML.
-// Use a loop to create a DOM tree for each journal entry in the data model -
-//and append it to the page when the 'DOMContentLoaded' event is fired.
-// Remove the example HTML entries once the user's entries are successfully being appended to the DOM on page load.
-// Add a navigation item to the navbar for Entries.
-// Add an anchor to the entries view that shows the entry form.
-// Ensure that submitoting a new journal entry also creates a new DOM tree for it and adds it to the page.
-// Ensure that submitting a new jurnal entry automatically shows the 'entries' view without reloading the page.
-
-/**      <ul id="unordered-list" class="unorderedentries">
-        <li class="row">
-          <img class="imgexample column-half borderradius margintop" src="images/placeholder-image-square.jpg"
-           alt="placeholder">
-          <div class="column-half">
-            <h1 class="entryheading">Test</h1>
-            <p class="marginleftlist">dddddd</p>
-          </div>
-        </li>
-      </ul>
-**/
