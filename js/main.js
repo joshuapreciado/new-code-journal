63bf3638f60f63e2e9913e513aa29a289aa0354c
joshuapreciado <preciadojoshua340@yahoo.com>
2022-07-26 21:51:00 -0700

    Pre-population complete

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
 //Listen for input events & Change src attribute of img element
 var photoholder = document.querySelector('.phototext');
 var fakeimage = document.querySelector('.standard-image')
+var titleTextvalue = document.querySelector('#titletextid');
 var formelement = document.querySelector('form');
 var viewentry = document.querySelector('ul');
+var phototextid = document.querySelector('#phototextid');
+var textareaid = document.querySelector('#textareaid');
 
 var headerOneEntries = document.querySelector('h1');
 
@@ -24,19 +27,36 @@ function falseyimage(url) {
 var entryform = document.querySelector('#entry-form');
 entryform.addEventListener('submit', function (event) {
   event.preventDefault();
-  var inputvalues = {};
-  inputvalues.titleText = formelement.elements.titleText.value;
-  inputvalues.photoText = formelement.elements.photoText.value;
-  inputvalues.textArea1 = formelement.elements.textArea1.value;
+  if (data.editing === null) {
+    var inputvalues = {};
+    inputvalues.titleText = formelement.elements.titleText.value;
+    inputvalues.photoText = formelement.elements.photoText.value;
+    inputvalues.textArea1 = formelement.elements.textArea1.value;
 
-  inputvalues.entryId = data.nextEntryId;
-  var testnewentry = newentries(inputvalues);
-  viewentry.prepend(testnewentry);
-  data.nextEntryId++;
-  data.entries.unshift(inputvalues);
-  fakeimage.src = 'images/placeholder-image-square.jpg';
-  formelement.reset();
-  viewswap('entries');
+    inputvalues.entryId = data.nextEntryId;
+    var testnewentry = newentries(inputvalues);
+    viewentry.prepend(testnewentry);
+    data.nextEntryId++;
+    data.entries.unshift(inputvalues);
+    fakeimage.src = 'images/placeholder-image-square.jpg';
+    formelement.reset();
+    viewswap('entries');
+  }
+  else {
+    inputvalues = {};
+    inputvalues.titleText = formelement.elements.titleText.value;
+    inputvalues.photoText = formelement.elements.photoText.value;
+    inputvalues.textArea1 = formelement.elements.textArea1.value;
+    inputvalues.entryId = data.editing.entryId;
+
+    data.entries[data.entries.length - inputvalues.entryId] = inputvalues;
+    viewentry.innerHTML = '';
+    testentries();
+    formelement.reset();
+    fakeimage.src = 'images/placeholder-image-square.jpg';
+    viewswap();
+    data.editing = null;
+  }
 })
 
 
@@ -79,7 +99,7 @@ function newentries(entry) {
 }
 
 //DomContentLoaded event listener
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener('DOMContentLoaded', function testentries(event) {
   for (var i = 0; i < data.entries.length; i++) {
     var entrylist = newentries(data.entries[i])
     viewentry.appendChild(entrylist)
@@ -113,17 +133,23 @@ newentry.addEventListener('click', function (event) {
 
 viewentry.addEventListener('click', function (event) {
   if (event.target.matches('.fas')) {
-    document.getElementById('newentry1').textContent = 'test edit';
+    document.getElementById('newentry1').textContent = 'Edit Entry';
     data.view = 'entry-form';
     viewswap('entry-form');
-    var testicon = event.target;
-    console.log(testicon.getAttribute('data-entry-id'));
-    data.editing = data.entries[data.entries.length - event.target.id]
-    console.log('test:', data.editing);
-    document.getElementById('titleid').setAttribute('value', data.editing.titleText);
 
+    var testicon = event.target.closest('li');
+    console.log(testicon);
+    var dataentryids = parseInt(testicon.getAttribute('data-entry-id'));
 
-    document.getElementById('phototextid').setAttribute('value', data.editing.photoText);
+    for (var v = 0; v < data.entries.length; v++) {
+      if (data.entries[v].entryId === dataentryids){
+        data.editing = data.entries[v];
+      }
+    }
+    console.log(data.editing);
+    document.getElementById('titletextid').setAttribute('value', data.editing.titleText)
+    document.getElementById('phototextid').setAttribute('value', data.editing.photoText)
+    document.getElementById('textareaid').textContent = data.editing.textArea1;
     fakeimage.src = data.editing.photoText;
   }
 });
